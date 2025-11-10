642861387e4ca2932d1ad9e1bb0bf44d967fb832
YoungDo-KIM0707 <you@example.com>
2025-11-10 18:25:48 +0900

    업데이트 내용 간단히 적기

--- src/Privacy.jsx
+++ src/Privacy.jsx
@@ -6,7 +6,7 @@ export default function Privacy() {
       <Header />
       <main className="mx-auto max-w-3xl p-6">
         <h1 className="text-2xl font-bold">개인정보 처리방침</h1>
-        <p className="mt-4 text-gray-700">여기에 개인정보 처리방침 내용을 넣으세요.</p>
+        
       </main>
     </div>
   );
--- src/Terms.jsx
+++ src/Terms.jsx
@@ -9,7 +9,7 @@ export default function Terms() {
       {/* 본문만 폭 제한 */}
      <main className="mx-auto max-w-3xl p-6">
         <h1 className="text-2xl font-bold">이용약관</h1>
-       <p className="mt-4 text-gray-700">여기에 이용약관 내용을 넣으세요.</p>
+       
       </main>
     </div>
   );
